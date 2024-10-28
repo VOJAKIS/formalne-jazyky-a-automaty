ede3b60ce49396e52d2a80bcbcd5595904b4f155
Adam Valašťan <valastan1@gmail.com>
2024-10-28 13:30:35 +0100

    Updated script

--- mvn_command.sh
+++ mvn_command.sh
@@ -1,6 +1,9 @@
 
-mvn archetype:generate 
-	-DgroupId=sk.umb.fpv.valastan.formalne_jazyky
-	-DartifactId=zadanie1
-	-DarchetypeArtifactId=maven-template
+group_id="sk.umb.fpv.valastan.formalne_jazyky"
+artifact_id="zadanie1"
+
+mvn archetype:generate \
+	-DgroupId=${group_id} \
+	-DartifactId=${artifact_id} \
+	-DarchetypeArtifactId=maven-template \
 	-DinteractiveMode=false
